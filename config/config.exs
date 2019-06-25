33d1098771aaa5c47fedcadcb7138ec3fbd46586
lambdanerd <hello.lambda.nerd@gmail.com>
2019-06-25 18:51:16 -0500

    Remove everything related to ueberauth_facebook

--- config/config.exs
+++ config/config.exs
@@ -31,10 +31,6 @@ config :ueberauth, Ueberauth.Strategy.Github.OAuth,
   client_id: System.get_env("GITHUB_CLIENT_ID"),
   client_secret: System.get_env("GITHUB_CLIENT_SECRET")
 
-config :ueberauth, Ueberauth.Strategy.Facebook.OAuth,
-  client_id: System.get_env("FACEBOOK_CLIENT_ID"),
-  client_secret: System.get_env("FACEBOOK_CLIENT_SECRET")
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
--- mix.exs
+++ mix.exs
@@ -32,8 +32,7 @@ defmodule Discuss.Mixfile do
         :postgrex,
         :envy,
         :ueberauth,
-        :ueberauth_github,
-        :ueberauth_facebook
+        :ueberauth_github
       ]
     ]
   end
@@ -57,8 +56,7 @@ defmodule Discuss.Mixfile do
       {:cowboy, "~> 1.0"},
       {:envy, "~> 1.1.1"},
       {:ueberauth, "~> 0.3"},
-      {:ueberauth_github, "~> 0.4"},
-      {:ueberauth_facebook, "~> 0.8"}
+      {:ueberauth_github, "~> 0.4"}
     ]
   end
 
