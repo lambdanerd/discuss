695dfc19b00d12aa2bf8bf5cc05585557e2724d9
lambdanerd <hello.lambda.nerd@gmail.com>
2019-06-25 09:43:43 -0500

    Create pipeline for Ueberauth, add to /auth scope

--- web/router.ex
+++ web/router.ex
@@ -2,28 +2,33 @@ defmodule Discuss.Router do
   use Discuss.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
+  end
+
+  pipeline :auth do
+    plug(Ueberauth)
   end
 
   scope "/", Discuss do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    resources "/", TopicController, except: [:show]
+    resources("/", TopicController, except: [:show])
   end
 
   scope "/auth", Discuss do
-    pipe_through :browser
+    pipe_through([:browser, :auth])
 
-    get "/:provider", AuthController, :request
-    get "/:provider/callback", AuthController, :callback
+    get("/:provider", AuthController, :request)
+    get("/:provider/callback", AuthController, :callback)
   end
 
   # Other scopes may use custom stacks.
