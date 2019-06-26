5e36ae1aa43a5a1efa7a19d62dfaeff2f5842d55
lambdanerd <hello.lambda.nerd@gmail.com>
2019-06-25 20:00:59 -0500

    Add has_many topics to User and migrate db

--- web/models/topic.ex
+++ web/models/topic.ex
@@ -2,7 +2,8 @@ defmodule Discuss.Topic do
   use Discuss.Web, :model
 
   schema "topics" do
-    field :title, :string
+    field(:title, :string)
+    belongs_to(:user, Disucss.User)
   end
 
   def changeset(struct, params \\ %{}) do
