22dfad7770b2ec4aa27bc98aeea7126aaf7f9009
lambdanerd <hello.lambda.nerd@gmail.com>
2019-06-27 23:07:51 -0500

    Implement the remaining pieces for comments. All done!

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -5,11 +5,27 @@ defmodule Discuss.CommentsChannel do
     topic =
       Discuss.Topic
       |> Repo.get(String.to_integer(topic_id))
+      |> Repo.preload(comments: [:user])
 
-    {:ok, %{}, socket}
+    {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
   end
 
-  def handle_in(name, message, socket) do
-    {:reply, :ok, socket}
+  def handle_in(_name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+    user_id = socket.assigns.user_id
+
+    changeset =
+      topic
+      |> build_assoc(:comments, user_id: user_id)
+      |> Discuss.Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        broadcast!(socket, "comments:#{socket.assigns.topic.id}:new", %{comment: comment})
+        {:reply, :ok, socket}
+
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -3,9 +3,9 @@
 
 // To use Phoenix channels, the first step is to import Socket
 // and connect at the socket path in "lib/my_app/endpoint.ex":
-import { Socket } from "phoenix";
+import { Socket } from 'phoenix';
 
-let socket = new Socket("/socket", { params: { token: window.userToken } });
+let socket = new Socket('/socket', { params: { token: window.userToken } });
 
 // When you connect, you'll often need to authenticate the client.
 // For example, imagine you have an authentication plug, `MyAuth`,
@@ -58,12 +58,51 @@ const createSocket = topicId => {
   let channel = socket.channel(`comments:${topicId}`, {});
   channel
     .join()
-    .receive("ok", resp => {
-      console.log("Joined successfully", resp);
+    .receive('ok', resp => {
+      console.log(resp);
+      renderComments(resp.comments);
     })
-    .receive("error", resp => {
-      console.log("Unable to join", resp);
+    .receive('error', resp => {
+      console.log('Unable to join', resp);
     });
+
+  channel.on(`comments:${topicId}:new`, renderComment);
+
+  document.querySelector('button').addEventListener('click', () => {
+    const content = document.querySelector('textarea').value;
+
+    channel.push('comment:add', { content: content });
+  });
 };
+
+function renderComments(comments) {
+  const renderedComments = comments.map(comment => {
+    return commentTemplate(comment);
+  });
+
+  document.querySelector('.collection').innerHTML = renderedComments.join('');
+}
+
+function renderComment(event) {
+  const renderedComment = commentTemplate(event.comment);
+
+  document.querySelector('.collection').innerHTML += renderedComment;
+}
+
+function commentTemplate(comment) {
+  let email = 'Anonymous';
+  if (comment.user) {
+    email = comment.user.email;
+  }
+
+  return `
+    <li class="collection-item">
+      ${comment.content}
+      <div class="secondary-content">
+        ${email}
+      </div>
+    </li>
+  `;
+}
 
 window.createSocket = createSocket;
